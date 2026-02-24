686d619f1e1ca7bb9ebaca9f8e637aaa1c18fcfe
salomecastilla12 <scastillab@unal.edu.co>
2026-02-24 00:13:22 -0500

    Add files via upload

---
+++ ACTIVIDAD 2/EJERCICIO2.1/Persona.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio21;
+
+/**
+ *
+ * @author Salome Castilla
+ */
+public class Persona {
+  
+    String nombre;
+    String apellidos;
+    String numeroDocumentoIdentidad;
+    int anoNacimiento;
+    
+    Persona(String nombre, String apellidos, String numeroDocumentoIdentidad, int anoNacimiento){
+        
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.numeroDocumentoIdentidad=numeroDocumentoIdentidad;
+        this.anoNacimiento=anoNacimiento;
+        
+    }
+ void imprimir(){
+     System.out.println("Nombre = " + nombre);
+     System.out.println("Apellidos = " + apellidos);
+     System.out.println("Numero de documento de identidad = "+ numeroDocumentoIdentidad);
+     System.out.println("Año de nacimiento = " + anoNacimiento);
+     
+ }   
+}
+    
+
