7003c5bc3b52dbf0cdd745b261b47acf444f8983
salomecastilla12 <scastillab@unal.edu.co>
2026-02-24 00:15:24 -0500

    Add files via upload

---
+++ ACTIVIDAD 2/EJERCICIO 2.2/tipoplaneta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.ejercicio22;
+
+/**
+ *
+ * @author Salome Castilla
+ */
+public enum tipoplaneta {
+    GASEOSO, TERRESTRE, ENANO
+}
