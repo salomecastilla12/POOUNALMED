0abac8645b9342dc967fae157b6a1f89d9adfd40
salomecastilla12 <scastillab@unal.edu.co>
2026-07-29 21:59:42 -0500

    Add files via upload

---
+++ ACTIVIDAD 6/EJERCICIO 4.6/Profesores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.profesores;
+
+/**
+ *
+ * @author Salome Castilla
+ */
+public class Profesores {
+
+protected void imprimir() {
+        System.out.println("Es un profesor.");
+    }
+}
